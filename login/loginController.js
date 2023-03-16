1af812a23c544106460042c50dcedf1977275438
Ivarsky <iamh90@gmail.com>
2023-03-16 20:42:40 +0000

    refactor: spinner out from login

--- login/loginController.js
+++ login/loginController.js
@@ -1,4 +1,4 @@
-import { buildSpinnerView } from "../advert-list/advertView.js";
+import { buildSpinnerView } from "../advert-list/advertView.js"; //TODO: spinner
 import { pubSub } from "../pubSub.js";
 import { isEmailValid } from "../utils/isEmailValid.js";
 import { loginUser } from "./login.js";
@@ -7,9 +7,8 @@ export function loginController(loginElement){
 
     loginElement.addEventListener('submit', (event)=> {
         event.preventDefault();
-
+        
         const emailElement = loginElement.querySelector('#username');
-
 
         if (!isEmailValid(emailElement.value)) {
             pubSub.publish(pubSub.TOPICS.SHOW_NOTIFICATION, 'Wrong email format!')
@@ -23,16 +22,15 @@ export function loginController(loginElement){
         const username = formData.get('username');
         const password = formData.get('password');
 
+
             try {
                 const jwt = await loginUser(username, password);
                 localStorage.setItem('token', jwt);
                 window.location = '/';
             } catch (error) {
                 pubSub.publish(pubSub.TOPICS.SHOW_NOTIFICATION, error.message)
+            } finally {
+                hideSpinner(loginElement)
             }
     }
-}
-function hideSpinner(loginElement) {
-    const spinnerElement = loginElement.querySelector('.spinner');
-    spinnerElement.classList.add('hide');
 }
