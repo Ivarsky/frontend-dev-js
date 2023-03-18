50794693fb000c09ee62f45a626a6077303358c9
Ivarsky <iamh90@gmail.com>
2023-03-18 20:38:40 +0000

    feat: logout button

--- user-actions/userActionsController.js
+++ user-actions/userActionsController.js
@@ -3,19 +3,24 @@ import { buildGreetings } from './userActionsView.js';
 
 export function userActionController(userActionsElement){
     const token = localStorage.getItem('token')
-
+    const logoutSessionElement = userActionsElement.querySelector('#logoutSession');
+    
     if (token) {
         const loginLinkElement = userActionsElement.querySelector('#loginLink');
         const signupLinkElement = userActionsElement.querySelector('#signupLink');
         loginLinkElement.remove();
         signupLinkElement.remove();
-
+        
         const payload = decodeToken(token);
         userActionsElement.appendChild(buildGreetings(payload.username));
+        logoutSessionElement.addEventListener('click', () => {
+            localStorage.removeItem('token')
+            window.location.reload()
+        })
     } else {
         const createAdLinkElement = userActionsElement.querySelector('#createAdLink');
         createAdLinkElement.remove()
-        //TODO: boton logout 
+        logoutSessionElement.remove()
 
     }
 }
