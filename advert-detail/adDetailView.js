58aac3a840cceeda90e005aa489e5b192a8166e5
Ivarsky <iamh90@gmail.com>
2023-03-19 15:07:55 +0000

    feat: sell or buy title to advert detail

--- advert-detail/adDetailView.js
+++ advert-detail/adDetailView.js
@@ -1,6 +1,14 @@
 export const  buildAdvertDetailView = (advert) => {
+    let sellOrBuy = ''
+    if (advert.sell === 'true') {
+        sellOrBuy = 'Selling'
+    } else {
+        sellOrBuy = 'Buying'
+    }
+
     return`
     <div class="add-info"">
+        <h2>${sellOrBuy}</h2>
         <span> ${advert.name} - ${advert.price} Euros </span>
         <br>
         <img src="${advert.image}" />
